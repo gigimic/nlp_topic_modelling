6aba8064df6dff2fda3800d99d600539760c6014
gigimic <gigimic@gmail.com>
2021-05-25 11:34:07 +0530

    create dictionary

--- topic_model.py
+++ topic_model.py
@@ -45,19 +45,44 @@ def preprocess(text):
 
 # Preview a document after preprocessing
 
-document_num = 1001
-doc_sample = documents[documents['index'] == document_num].values[0][0]
+# document_num = 1001
+# doc_sample = documents[documents['index'] == document_num].values[0][0]
 
-print("Original document: ")
-words = []
-for word in doc_sample.split(' '):
-    words.append(word)
-print(words)
-print("\n\nTokenized and lemmatized document: ")
-print(preprocess(doc_sample))
+# print("Original document: ")
+# words = []
+# for word in doc_sample.split(' '):
+#     words.append(word)
+# print(words)
+# print("\n\nTokenized and lemmatized document: ")
+# print(preprocess(doc_sample))
 
 # Now  use the map function from pandas to apply preprocess() to the headline_text column
 # so that we can preprocess all the headlines
 processed_docs = documents['headline_text'].map(preprocess)
 
-print(processed_docs[:10])
+# print(processed_docs[:10])
+'''
+Create a dictionary from 'processed_docs' containing the number of times a word appears 
+in the training set using gensim.corpora.Dictionary and call it 'dictionary'
+'''
+dictionary = gensim.corpora.Dictionary(processed_docs)
+
+# checking the first 10 words in the dictionary 
+count = 0
+for k, v in dictionary.iteritems():
+    print(k, v)
+    count += 1
+    if count > 10:
+        break
+
+print(len(dictionary))
+
+'''
+OPTIONAL STEP
+Remove very rare and very common words:
+
+- words appearing less than 15 times
+- words appearing in more than 10% of all documents
+'''
+# TODO: apply dictionary.filter_extremes() with the parameters mentioned above
+dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n=100000)
